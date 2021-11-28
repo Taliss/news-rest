cf57c045759ba8ce102c6021f70fd15f5c45e08a
Iliyan <iliyan.vlahov@gmail.com>
2021-11-28 13:11:38 +0200

    WIP: integration tests for update

--- tests/news/update.test.js
+++ tests/news/update.test.js
@@ -18,7 +18,8 @@ beforeAll(async () => {
   try {
     await establishDBConnection();
     // create one item and get the id
-    const insertedIds = await news.create([validNews]);
+    // create is mutating the input to insert Ids, so be carefull!
+    const insertedIds = await news.create([{ ...validNews }]);
     validId = insertedIds[0];
   } catch (err) {
     console.error('Unable to run tests duo to error while doing test setup');
@@ -35,11 +36,75 @@ describe('Updating news document with PUT', () => {
   test('should not pass when invalid id is passed', async () => {
     await request(app.callback())
       .put(`/api/news/somethingAsAnId`)
+      .send({
+        title: 'i am not a valid news record, but id will fail before me',
+      })
       .expect(400)
       .expect((res) => {
         expect(res.body.message).toMatch(
           /fails to match the valid mongo id pattern/
         );
+      });
+  });
+
+  test('should not pass when partial data is send', async () => {
+    await request(app.callback())
+      .put(`/api/news/${validId}`)
+      .send({
+        title: 'missing description and text',
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.message).toMatch(/is required/);
+      });
+  });
+
+  test('should not pass when extra prop is send', async () => {
+    await request(app.callback())
+      .put(`/api/news/${validId}`)
+      .send({
+        foo: 'foo',
+        title: 'missing description and text',
+        description: 'valid',
+        text: '1234567890',
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.message).toMatch(/is not allowed/);
+      });
+  });
+
+  // same validation as for creating, skipping other test scenarious as this is already tested
+  test('should not pass when all props are send, but one or more are invalid', async () => {
+    await request(app.callback())
+      .put(`/api/news/${validId}`)
+      .send({
+        title: 'missing description and text',
+        description: '1',
+        text: '1234567890',
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.message).toMatch(/must be at least/);
+      });
+  });
+
+  test('should update the record', async () => {
+    const newTitle = 'updated title only';
+    await request(app.callback())
+      .put(`/api/news/${validId}`)
+      .send({ ...validNews, title: newTitle })
+      .expect(200);
+
+    await request(app.callback())
+      .get(`/api/news/${validId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toEqual({
+          ...validNews,
+          title: newTitle,
+          _id: validId,
+        });
       });
   });
 
